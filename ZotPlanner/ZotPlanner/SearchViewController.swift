c73b780c98609bdbd769c4702002c49be4835e96
HumzaMKhan <humzamk@uci.edu>
2021-05-23 17:03:46 -0700

    Added outlets for search fields

---
+++ ZotPlanner/ZotPlanner/SearchViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SearchViewController.swift
+//  ZotPlanner
+//
+//  Created by Humza Khan on 5/23/21.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    @IBOutlet weak var termPicker: UIPickerView!
+    @IBOutlet weak var departmentPicker: UIPickerView!
+    @IBOutlet weak var GEPicker: UIPickerView!
+    @IBOutlet weak var courseNumberField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
