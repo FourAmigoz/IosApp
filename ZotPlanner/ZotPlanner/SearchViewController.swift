3e4253265a08538b4655240327b2527c52e41bb2
HumzaMKhan <humzamk@uci.edu>
2021-05-23 22:34:53 -0700

    Added ability to pick from a list of terms and departments

--- ZotPlanner/ZotPlanner/SearchViewController.swift
+++ ZotPlanner/ZotPlanner/SearchViewController.swift
@@ -6,19 +6,74 @@
 //
 
 import UIKit
+import Alamofire
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
     @IBOutlet weak var termPicker: UIPickerView!
     @IBOutlet weak var departmentPicker: UIPickerView!
     @IBOutlet weak var GEPicker: UIPickerView!
-    @IBOutlet weak var courseNumberField: UITextField!
+    
+    let APIURL = "https://damp-brushlands-74834.herokuapp.com/"
+    let possibleTerms = [
+        "2021 Fall",
+        "2021 Spring"
+    ]
+    let possibleDepartments = [
+        "EECS",
+        "ICS",
+        "CS"
+    ]
+    
+    // The number of picker components
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Returns the number of rows in a given picker
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == termPicker {
+            return possibleTerms.count
+        } else if pickerView == departmentPicker {
+            return possibleDepartments.count
+        } else {
+            return 1
+        }
+    }
+    
+    // Returns the String for a given row in a given picker
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == termPicker {
+            return possibleTerms[row]
+        } else if pickerView == departmentPicker {
+            return possibleDepartments[row]
+        } else {
+            return ""
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        termPicker.dataSource = self
+        termPicker.delegate = self
+        departmentPicker.dataSource = self
+        departmentPicker.delegate = self
+        
+        let parameters = [
+            "term": "2019 Fall",
+            "GE": "GE-2",
+            "instructorName": "Pattis"
+        ]
+        AF.request(APIURL, method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+            print(response)
+        }
     }
     
+    // Call the API when the user clicks on search
+    @IBAction func callAPI(_ sender: Any) {
+        
+    }
 
     /*
     // MARK: - Navigation
@@ -31,3 +86,5 @@ class SearchViewController: UIViewController {
     */
 
 }
+
+
--- ZotPlanner/ZotPlanner/ViewController.swift
+++ ZotPlanner/ZotPlanner/ViewController.swift
@@ -6,16 +6,13 @@
 //
 
 import UIKit
-import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        AF.request("http://localhost:8081").response { response in
-            debugPrint(response)
-        }
+        
     }
 
 
