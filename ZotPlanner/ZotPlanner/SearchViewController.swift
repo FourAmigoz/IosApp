a949d70cd75d174bcfb9835011f062c2dda21b61
Jimmy Juarez <jimmyaj@uci.edu>
2021-06-01 10:18:03 -0700

    Add 3 working pickeview for landing page

--- ZotPlanner/ZotPlanner/SearchViewController.swift
+++ ZotPlanner/ZotPlanner/SearchViewController.swift
@@ -1,112 +1,88 @@
-//
-//  SearchViewController.swift
-//  ZotPlanner
-//
-//  Created by Humza Khan on 5/23/21.
-//
-
 import UIKit
-import Alamofire
-import PickerView
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-    @IBOutlet weak var termPicker: PickerView!
-    @IBOutlet weak var departmentPicker: PickerView!
-    @IBOutlet weak var gePicker: PickerView!
+    @IBOutlet weak var schoolTerm: UITextField!
+    @IBOutlet weak var schoolDepartment: UITextField!
+    @IBOutlet weak var schoolGE: UITextField!
     
     let APIURL = "https://damp-brushlands-74834.herokuapp.com/"
-    let possibleTerms = [
-        "2021 Fall",
-        "2021 Spring"
-    ]
-    let possibleDepartments = [
-        "EECS",
-        "ICS",
-        "CS"
-    ]
+
+    let departments = ["EECS","ICS","CS"]
+    let terms = [ "2021 Fall","2021 Spring"]
+    let genEduc = ["GE-1A","GE-1B","GE-2","GE-3","GE-4","GE-5A","GE-5B","GE-6","GE-7","GE-8"]
+    
+    var termPickerView = UIPickerView()
+    var departmentPickerView = UIPickerView()
+    var gePickerView = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        termPicker.dataSource = self
-        termPicker.delegate = self
         
-        let scrollingStyle: PickerView.ScrollingStyle
-        let selectionStyle: PickerView.SelectionStyle
+        schoolTerm.inputView = termPickerView
+        schoolDepartment.inputView = departmentPickerView
+        schoolGE.inputView = gePickerView
         
-        termPicker.scrollingStyle = PickerView.ScrollingStyle(rawValue: 0)!
-        termPicker.selectionStyle = PickerView.SelectionStyle(rawValue: 0)!
-        
-//        if let currentSelected = currentSelectedValue, let indexOfCurrentSelectedValue = numbers.index(of: currentSelected) {
-//            examplePicker.currentSelectedRow = indexOfCurrentSelectedValue
-//        }
+        termPickerView.dataSource = self
+        termPickerView.delegate = self
+        departmentPickerView.dataSource = self
+        departmentPickerView.delegate = self
+        gePickerView.dataSource = self
+        gePickerView.delegate = self
         
         
-//        departmentPicker.dataSource = self
-//        departmentPicker.delegate = self
+        termPickerView.tag = 1
+        departmentPickerView.tag = 2
+        gePickerView.tag = 3
         
-//        let parameters = [
-//            "term": "2019 Fall",
-//            "GE": "GE-2",
-//            "instructorName": "Pattis"
-//        ]
-//        AF.request(APIURL, method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
-//            print(response)
-//        }
+        schoolGE.textAlignment = .center
+        schoolDepartment.textAlignment = .center
+        schoolTerm.textAlignment = .center
     }
     
-    // Call the API when the user clicks on search
-    @IBAction func callAPI(_ sender: Any) {
-        
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
 
-}
-
-extension SearchViewController: PickerViewDataSource {
-    func pickerViewNumberOfRows(_ pickerView: PickerView) -> Int {
-        if pickerView == termPicker {
-            return possibleTerms.count
-//        } else if pickerView == departmentPicker {
-//            return possibleDepartments.count
-        } else {
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView.tag {
+        case 1:
+            return terms.count
+        case 2:
+            return departments.count
+        case 3:
+            return genEduc.count
+        default:
             return 1
         }
     }
-    
-    func pickerView(_ pickerView: PickerView, titleForRow row: Int) -> String {
-        if pickerView == termPicker {
-            return possibleTerms[row]
-//        } else if pickerView == departmentPicker {
-//            return possibleDepartments[row]
-        } else {
-            return ""
-        }
-    }
-}
 
-extension SearchViewController: PickerViewDelegate {
-    func pickerViewHeightForRows(_ pickerView: PickerView) -> CGFloat {
-        return 50.0
-    }
-    
-    func pickerView(_ pickerView: PickerView, didSelectRow row: Int) {
-        print("The selected item is \(possibleTerms[row])")
-    }
-    
-    func pickerView(_ pickerView: PickerView, didTapRow row: Int) {
-        print("The row \(row) was tapped by the user")
-    }
-    
-    func pickerView(_ pickerView: PickerView, styleForLabel label: UILabel, highlighted: Bool) {
-        label.textAlignment = .center
-        
-        if highlighted {
-            label.font = UIFont.systemFont(ofSize: 25.0)
-            label.textColor = view.tintColor
-        } else {
-            label.font = UIFont.systemFont(ofSize: 15.0)
-            label.textColor = .lightGray
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+        case 1:
+            return terms[row]
+        case 2:
+            return departments[row]
+        case 3:
+            return genEduc[row]
+        default:
+            return "Data not found"
         }
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch pickerView.tag {
+        case 1:
+            schoolTerm.text = terms[row]
+            schoolTerm.resignFirstResponder()
+        case 2:
+            schoolDepartment.text = departments[row]
+            schoolDepartment.resignFirstResponder()
+        case 3:
+            schoolGE.text = genEduc[row]
+            schoolGE.resignFirstResponder()
+        default:
+            return
+        }       
     }
 }
--- ZotPlanner/ZotPlannerTests/ZotPlannerTests.swift
+++ ZotPlanner/ZotPlannerTests/ZotPlannerTests.swift
@@ -1,33 +1,33 @@
+////
+////  ZotPlannerTests.swift
+////  ZotPlannerTests
+////
+////  Created by Humza Khan on 5/18/21.
+////
 //
-//  ZotPlannerTests.swift
-//  ZotPlannerTests
+//import XCTest
+//@testable import ZotPlanner
 //
-//  Created by Humza Khan on 5/18/21.
+//class ZotPlannerTests: XCTestCase {
 //
-
-import XCTest
-@testable import ZotPlanner
-
-class ZotPlannerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
-}
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+//
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+//
+//}
